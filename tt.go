3a168e02e440f3ce2bcce8d4c8b658d1b7420a73
Charles Neau <charles63500@gmail.com>
2018-10-02 16:22:04 +0100

    that is proper onelining still readable

--- tt.go
+++ tt.go
@@ -12,9 +12,7 @@ var log = logger.New(os.Stdout, "[TRACK] ", logger.LstdFlags)
 // Track allows you to track timing of a function
 // Just add defer tt.Track(time.New, "nameOfFunction")
 // at the top of your function.
-func Track(start time.Time, name string) {
-	log.Printf("<[%s]> %s", name, time.Since(start))
-}
+func Track(start time.Time, name string) { log.Printf("<[%s]> %s", name, time.Since(start)) }
 
 // T allows you to track time of a function
 // It will get the name of the function via runtime
@@ -23,7 +21,5 @@ func T() func() {
 	pc, _, _, _ := runtime.Caller(1)
 	name := runtime.FuncForPC(pc).Name() // nil is ok, see impl of Name()
 	start := time.Now()
-	return func() {
-		Track(start, name)
-	}
+	return func() { Track(start, name) }
 }
